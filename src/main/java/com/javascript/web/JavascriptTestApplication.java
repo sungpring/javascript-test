ebf2e4acf6d6d985d3c614c81a6e58bf634de962
KY <sungpring@naver.com>
2021-12-18 20:59:37 +0900

    test

---
+++ src/main/java/com/javascript/web/JavascriptTestApplication.java
@@ -0,0 +1,13 @@
+package com.javascript.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavascriptTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavascriptTestApplication.class, args);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,14 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page trimDirectiveWhitespaces="true" %>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Insert title here</title>
+<meta charset="UTF-8" />
+</head>
+<body>
+123
+</body>
+</html>
